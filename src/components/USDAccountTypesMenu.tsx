d21405ad7211f8b2fc0bdc562fcec34244e0ef8f
Tim Rogers <me@timrogers.co.uk>
2023-07-23 22:31:17 +0100

    Fix menu highlighting

--- src/components/USDAccountTypesMenu.tsx
+++ src/components/USDAccountTypesMenu.tsx
@@ -1,29 +1,44 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import classnames from "classnames";
 
 const USDAccountTypesMenu = () => {
+  const router = useRouter();
+
+  const classesForLink = (pathname: string) => {
+    return classnames({
+      ["py-3"]: true,
+      "hover:bg-green-700": true,
+      "border-b": true,
+      "active:bg-green-700": true,
+      block: true,
+      "bg-green-700": router.pathname === pathname,
+    });
+  };
+
   return (
-    <div className="width-full bg-green-500 md:columns-4 text-center text-white">
+    <div className="width-full bg-green-500 md:columns-4 text-center text-white gap-0">
       <Link
         href="/usd-savings-accounts"
-        className="p-3 hover:bg-green-700 border-b active:bg-green-700 block"
+        className={classesForLink("/usd-savings-accounts")}
       >
         All
       </Link>
       <Link
         href="/usd-savings-accounts/instant-access"
-        className="p-3 border-b hover:bg-green-700 active:bg-green-700 block"
+        className={classesForLink("/usd-savings-accounts/instant-access")}
       >
         Instant access
       </Link>
       <Link
         href="/usd-savings-accounts/notice"
-        className="p-3 hover:bg-green-700 border-b active:bg-green-700 block"
+        className={classesForLink("/usd-savings-accounts/notice")}
       >
         Notice
       </Link>
       <Link
         href="/usd-savings-accounts/fixed-rate"
-        className="p-3 hover:bg-green-700 border-b active:bg-green-700 block"
+        className={classesForLink("/usd-savings-accounts/fixed-rate")}
       >
         Fixed rate
       </Link>

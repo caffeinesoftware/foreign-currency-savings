61b37aa6165b27d0bb9b1804fbdadde9b16e72be
Tim Rogers <me@timrogers.co.uk>
2023-07-25 19:00:49 +0100

    Add Google Analytics to the layout

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,11 +1,22 @@
+import Link from "next/link";
+import Script from "next/script";
 import { Tooltip } from "react-tooltip";
 
 import "./../app/globals.css";
-import Link from "next/link";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-83LZHDPR2Tx" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-83LZHDPR2T');
+        `}
+      </Script>
       <div className="width-full bg-green-700 p-3">
         <Link href="/">
           <h1 className="text-white font-bold">

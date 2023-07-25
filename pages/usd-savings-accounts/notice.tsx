622c392acb04353e9d7b4eef218c7a256eec478e
Tim Rogers <me@timrogers.co.uk>
2023-07-25 10:13:41 +0100

    Move amount filter to its own component

--- pages/usd-savings-accounts/notice.tsx
+++ pages/usd-savings-accounts/notice.tsx
@@ -9,6 +9,7 @@ import Layout from "./../layout";
 import USDAccountTypesMenu from "@/src/components/USDAccountTypesMenu";
 import { presentTerm } from "@/src/utils";
 import OfferList from "@/src/components/OfferList";
+import AmountFilter from "@/src/components/AmountFilter";
 
 interface IPageProps {
   noticePeriodOptions: number[];
@@ -75,28 +76,9 @@ export default function Currency({ noticePeriodOptions, offers }: IPageProps) {
           ))}
         </select>
 
-        <label
-          htmlFor="initialDeposit"
-          className="text-white my-2 block md:ml-4"
-        >
-          Amount
-        </label>
-
-        <input
-          name="initialDeposit"
-          className="block my-2 w-full text-lg ld:float-left md:ml-4 md:grow pl-5"
+        <AmountFilter
           onChange={onChangeInitialDeposit}
-          type="number"
-          min="1"
-          step="1"
-          placeholder="Any"
-          style={{
-            backgroundImage:
-              "url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' height='16px' width='85px'><text x='2' y='13' fill='gray' font-size='12' font-family='arial'>$</text></svg>",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "5px 2px",
-            backgroundSize: "auto 75%",
-          }}
+          value={initialDeposit}
         />
 
         <div className="clear-both"></div>
